c0183fa60da239451dd407dfc39051cbb0210ea7
5123 <krekish@list.ru>
2025-05-16 09:37:09 +0900

    13

---
+++ Data/Migrations/20250424060606_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LicenseNedroMVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Licenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    LicenseNumber = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Location = table.Column<string>(type: "TEXT", nullable: false),
+                    District = table.Column<string>(type: "TEXT", nullable: false),
+                    Activity = table.Column<string>(type: "TEXT", nullable: false),
+                    Latitude = table.Column<decimal>(type: "decimal(10, 6)", nullable: false),
+                    Longitude = table.Column<decimal>(type: "decimal(10, 6)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MineralType = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Reserves = table.Column<string>(type: "TEXT", nullable: false),
+                    Area = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Investment = table.Column<string>(type: "TEXT", nullable: false),
+                    PositionTop = table.Column<string>(type: "TEXT", nullable: false),
+                    PositionLeft = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Licenses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Licenses");
+        }
+    }
+}
